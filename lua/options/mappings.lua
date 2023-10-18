344854e5faf0e013dd4dcebb5eb79cadcf9bb96a
im <ilya.mikhailov4@gmail.com>
2023-10-18 19:05:11 +0300

    Moved vim options and keybindings to separate files

---
+++ lua/options/mappings.lua
@@ -0,0 +1,13 @@
+-- Close the current buffer
+vim.api.nvim_set_keymap('n', '<C-W>d', ':bd<CR>', { noremap = true, silent = true })
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Navigate buffers (tabs)
+vim.keymap.set("n", "<S-h>", "<cmd>bprev<cr>", { desc = "Previous buffer", silent = true })
+vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer", silent = true })
